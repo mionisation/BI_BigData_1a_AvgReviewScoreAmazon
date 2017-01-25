1c054b628478c1b1e71c6e77ebf1c5de8dc2ca63
mionisation <michael.antonius.ion@gmail.com>
2017-01-25 08:08:42 -0800

    write mapper and reducer driver

--- 1/src/main/java/AvgReviewScoreAmazon/ReviewScoreAvgDriver.java
+++ 1/src/main/java/AvgReviewScoreAmazon/ReviewScoreAvgDriver.java
@@ -2,6 +2,7 @@ package AvgReviewScoreAmazon;
 
 import java.io.IOException;
 import java.util.regex.Pattern;
+
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -11,10 +12,10 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-
 import org.apache.log4j.Logger;
 
 public class ReviewScoreAvgDriver extends Configured implements Tool {
@@ -35,40 +36,35 @@ public class ReviewScoreAvgDriver extends Configured implements Tool {
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(DoubleWritable.class);
     return job.waitForCompletion(true) ? 0 : 1;
   }
 
   //TODO DELETE THIS
-  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-    private long numRecords = 0;    
-    private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");
+  public static class Map extends Mapper<LongWritable, Text, Text, DoubleWritable> {   
 
     public void map(LongWritable offset, Text lineText, Context context)
         throws IOException, InterruptedException {
       String line = lineText.toString();
-      Text currentWord = new Text();
-      for (String word : WORD_BOUNDARY.split(line)) {
-        if (word.isEmpty()) {
-            continue;
-        }
-            currentWord = new Text(word);
-            context.write(currentWord,one);
-        }
+      String[] prArr = line.split(",");
+      String product = prArr[1].trim();
+      String rating = prArr[2].trim();
+      context.write(new Text(product), new DoubleWritable(Double.parseDouble(rating)));
     }
   }
 
-  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+  public static class Reduce extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
     @Override
-    public void reduce(Text word, Iterable<IntWritable> counts, Context context)
+    public void reduce(Text word, Iterable<DoubleWritable> counts, Context context)
         throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable count : counts) {
+      double sum = 0;
+      double amount = 0; 
+      for (DoubleWritable count : counts) {
         sum += count.get();
+        amount++;
       }
-      context.write(word, new IntWritable(sum));
+     sum /= amount;
+      context.write(word, new DoubleWritable(sum));
     }
   }
 }
